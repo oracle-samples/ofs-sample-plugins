669c35d743a60504ac717971af14e0bbdbf0535c
Miquel Gall <gall.miquel@gmail.com>
2024-01-18 11:15:08 -0500

    Adjusted to solve fs problems during webpack execution

--- samples/005-PreDebrief/webpack.config.js
+++ samples/005-PreDebrief/webpack.config.js
@@ -3,64 +3,65 @@ const webpack = require("webpack");
 const TerserPlugin = require("terser-webpack-plugin");
 
 var config = {
-    entry: "./assets/js/main.ts",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: ["ts-loader"],
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-        ],
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            process: "process/browser",
-            Buffer: ["buffer", "Buffer"],
-        }),
+  entry: "./assets/js/main.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["ts-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-        fallback: {
-            buffer: require.resolve("buffer"),
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    fallback: {
+      buffer: require.resolve("buffer"),
+      fs: false,
+    },
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          parse: {},
+          compress: {},
+          mangle: {
+            properties: {
+              regex: /^_/,
+            },
+          },
         },
-    },
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    parse: {},
-                    compress: {},
-                    mangle: {
-                        properties: {
-                            regex: /^_/,
-                        },
-                    },
-                },
-            }),
-        ],
-    },
+      }),
+    ],
+  },
 };
 
 module.exports = (env, argv) => {
-    if (argv.mode === "development") {
-        config.optimization.minimize = false;
-        config.devtool = "inline-source-map";
-    }
+  if (argv.mode === "development") {
+    config.optimization.minimize = false;
+    config.devtool = "inline-source-map";
+  }
 
-    if (argv.mode === "production") {
-        config.optimization.minimize = true;
-        config.devtool = undefined;
-    }
+  if (argv.mode === "production") {
+    config.optimization.minimize = true;
+    config.devtool = undefined;
+  }
 
-    return config;
+  return config;
 };
