19acef0bbdd79d7af7568924c4caa9b95915ffaf
Miquel Gall <gall.miquel@gmail.com>
2024-04-02 10:41:19 -0400

    First version of clock in clock out

---
+++ samples/008-ClockInOut/assets/js/main.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright © 2022, 2023, Oracle and/or its affiliates.
+ * Licensed under the Universal Permissive License (UPL), Version 1.0  as shown at https://oss.oracle.com/licenses/upl/
+ */
+
+import { CustomPlugin } from "./custom";
+
+export {};
+declare global {
+    interface Window {
+        ofs: CustomPlugin;
+    }
+}
+
+window.onload = function () {
+    window.ofs = new CustomPlugin("ClockInOut");
+};
