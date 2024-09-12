8c9cc3958fcb411a2f9928c923c0c66f5f73f2ae
Miquel Gall <gall.miquel@gmail.com>
2024-09-12 18:24:00 -0400

    First working version with ofs-user/proxy 1.9

--- samples/010-TokenExample/README.md
+++ samples/010-TokenExample/README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-This plugin as an example on how to use the getToken procedure
+This plugin as an example on how to use applications to access OFS APIs
 
 ## Parameters
 
@@ -22,7 +22,12 @@ none
 
 ## How to use
 
-Use this plugin as an example on how you could use the get token procedure to invoke OFS APIs
+Add this plugin as part of any screen.
+Add an application as part of the plugin configuration.
+Ensure that the application has access to the getSubscriptions API.
+If the application has been properly configured, it will return a popup window with the result of the getSubscriptions API
+If the application is missing, it will return an alert indicating that the proxy has not been created.
+If the application doesn't have the right permission configured, it will show the response in the popup window.
 
 ## For development
 
@@ -30,12 +35,12 @@ Use this plugin as an example on how you could use the get token procedure to in
 
 1. Install the dependencies
 
-   `npm install`
+    `npm install`
 
 2. Build and compress the plugin
 
-   `webpack --mode=production`
+    `webpack --mode=production`
 
 3. Create the zip file that can be uploaded to OFS
 
-   `cd dist; zip plugin.zip index.html main.js`
+    `cd dist; zip plugin.zip index.html main.js`
--- samples/010-TokenExample/assets/js/custom.ts
+++ samples/010-TokenExample/assets/js/custom.ts
@@ -4,78 +4,50 @@
  */
 
 declare global {
-  var initMessage: string;
-  var applicationKey: any;
-  var restUrl: any;
+    var initMessage: string;
+    var applicationKey: any;
+    var restUrl: any;
 }
 import {
-  OFSPlugin,
-  OFSMessage,
-  OFSOpenMessage,
-  OFSCallProcedureResultMessage,
+    OFSPlugin,
+    OFSMessage,
+    OFSOpenMessage,
+    OFSCallProcedureResultMessage,
 } from "@ofs-users/plugin";
+import { OFSSubscriptionResponse } from "@ofs-users/proxy";
 class OFSCustomOpenMessage extends OFSOpenMessage {
-  activity: any;
-  openParams: any;
-  resource: any;
+    activity: any;
+    openParams: any;
+    resource: any;
 }
 
 class OFSCustomInitMessage extends OFSMessage {
-  applications: any;
+    applications: any;
 }
 export class CustomPlugin extends OFSPlugin {
-  generateCallId() {
-    const randomBytes = new Uint8Array(16);
-    var randomValue = window.crypto.getRandomValues(randomBytes);
-  }
-  storeInitProperty(property: string, data: any) {
-    console.debug(`$${this.tag}.${property}: Storing ${property}`, data);
-    window.localStorage.setItem(`${this.tag}.${property}`, data);
-  }
-
-  getInitProperty(property: string): any {
-    var data = window.localStorage.getItem(`${this.tag}.${property}`);
-    return data;
-  }
-
-  init(message: OFSCustomInitMessage): void {
-    console.debug(`${this.tag} : INIT MESSAGE : ${JSON.stringify(message)}`);
-    this.storeInitProperty("initMessage", JSON.stringify(message));
-    console.debug(
-      `${this.tag} : GLOBAL DATA INIT : ${this.getInitProperty("initMessage")}`
-    );
-  }
-  open(data: OFSCustomOpenMessage) {
-    console.debug(`${this.tag} : OPEN: ${JSON.stringify(data)}`);
-    var initMessageStr: string = this.getInitProperty("initMessage");
-    console.debug(`${this.tag} : GLOBAL DATA OPEN: ${initMessageStr}`);
-    var plugin = this;
-    var initMessage: OFSCustomInitMessage = JSON.parse(initMessageStr);
-    var applicationKey = "";
-    var restUrl = "";
-    for (const [key, value] of Object.entries(initMessage.applications)) {
-      applicationKey = key;
-      var application: any = value;
-      restUrl = application.resourceUrl;
+    open(data: OFSCustomOpenMessage) {
+        console.debug(`${this.tag} : OPEN: ${JSON.stringify(data)}`);
+        var plugin = this;
+        if (this.proxy) {
+            this.getSubscriptions();
+        } else {
+            alert(
+                "Proxy not available. Review if you have included an application in the plugin configuration and it you have, review the logs for any errors."
+            );
+            this.close();
+        }
     }
-    globalThis.applicationKey = applicationKey;
-    globalThis.restUrl = restUrl;
-    var callId = `${this.generateCallId}`;
-    var callProcedureData = {
-      callId: callId,
-      procedure: "getAccessToken",
-      params: {
-        applicationKey: applicationKey,
-      },
-    };
-    plugin.callProcedure(callProcedureData);
-  }
-  callProcedureResult(data: OFSCallProcedureResultMessage) {
-    console.debug(
-      `${this.tag} : CALL PROCEDURE RESULT : ${JSON.stringify(data)}`
-    );
-    alert(
-      `This is where I would implement the logic to call OFS Calls to the URL ${globalThis.restUrl} with the token ${data.resultData.token}`
-    );
-  }
+    async getSubscriptions() {
+        const subscriptions_list: OFSSubscriptionResponse =
+            await this.proxy.getSubscriptions();
+        console.debug(
+            `${this.tag} : Subscriptions: ${JSON.stringify(subscriptions_list)}`
+        );
+        alert(
+            `Subscriptions captured successfully ${JSON.stringify(
+                subscriptions_list
+            )}`
+        );
+        this.close();
+    }
 }
