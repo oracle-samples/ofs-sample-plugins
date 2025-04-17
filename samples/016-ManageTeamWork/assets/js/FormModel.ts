45c414e3cc8b88cee0a4c71b76971c5b9945686c
Miquel Gall <miguel.gall@oracle.com>
2025-04-16 22:53:54 -0400

    Adding and removing elements to the table working

--- samples/016-ManageTeamWork/assets/js/FormModel.ts
+++ samples/016-ManageTeamWork/assets/js/FormModel.ts
@@ -1,40 +1,82 @@
 export class LaborFormModel {
+    private laborLines: HTMLTableElement;
     private laborForm: HTMLFormElement;
+    private formSection: HTMLElement;
+    private formTitle: HTMLElement;
     private technicianSelect: HTMLSelectElement;
     private laborTypeSelect: HTMLSelectElement;
     private laborItemDesc: any;
     private laborItemNumber: any;
-
+    private activityStartTimeDate: Date;
+    private activityEndTimeDate: Date;
+    private thisFormRow: number | null = null;
+    private formCellsMapping = {
+        "technician-name": {
+            type: "select",
+            index: 1,
+            editable: false,
+            uniqueKey: true,
+        },
+        "start-time": { type: "input", index: 2 },
+        "end-time": { type: "input", index: 3 },
+        "labor-type": {
+            type: "select",
+            index: 5,
+            editable: false,
+            uniqueKey: true,
+        },
+        "text-description": { type: "input", index: 6 },
+    };
     constructor(
+        laborLines: HTMLTableElement,
         laborForm: HTMLFormElement,
+        formSection: HTMLElement,
+        formTitle: HTMLElement,
         technicianSelect: HTMLSelectElement,
         laborTypeSelect: HTMLSelectElement,
         laborItemDesc: any,
-        laborItemNumber: any
+        laborItemNumber: any,
+        activityStartTimeDate: Date,
+        activityEndTimeDate: Date
     ) {
+        this.laborLines = laborLines;
         this.laborForm = laborForm;
+        this.formSection = formSection;
+        this.formTitle = formTitle;
         this.technicianSelect = technicianSelect;
         this.laborTypeSelect = laborTypeSelect;
         this.laborItemDesc = laborItemDesc;
         this.laborItemNumber = laborItemNumber;
+        this.activityStartTimeDate = activityStartTimeDate;
+        this.activityEndTimeDate = activityEndTimeDate;
     }
 
     // Reset the form to its initial state
-    resetForm(activityStartTimeDate: Date, activityEndTimeDate: Date): void {
-        this.laborForm.reset();
+    resetForm(): void {
+        this.trackStep("RESET", "START");
+        (this.laborForm as HTMLFormElement).reset();
         this.setInputValue(
             "start-time",
-            this.formatTime(activityStartTimeDate)
+            this.formatTime(this.activityStartTimeDate)
         );
-        this.setInputValue("end-time", this.formatTime(activityEndTimeDate));
+        this.setInputValue(
+            "end-time",
+            this.formatTime(this.activityEndTimeDate)
+        );
+        // Using all elements in formCellsMapping, make ALL of them editable
+        Object.entries(this.formCellsMapping).forEach(([id, config]) => {
+            this.makeElementEditable(id);
+        });
+        this.trackStep("RESET", "END");
     }
 
     // Populate the technician dropdown
     populateTechnicianDropdown(teamMembers: any, resource: any): void {
+        this.trackStep("POPULATE TECHNICIANS", "START");
         Object.entries(teamMembers).forEach(([key, member]: [string, any]) => {
             const option = document.createElement("option");
             option.value = key;
-            option.textContent = member.name;
+            option.textContent = member.pname;
             this.technicianSelect.appendChild(option);
         });
 
@@ -44,8 +86,17 @@ export class LaborFormModel {
             resourceOption.textContent = resource.pname;
             this.technicianSelect.appendChild(resourceOption);
         }
+        this.trackStep("POPULATE TECHNICIANS", "END");
     }
-
+    trackStep(action: string, step: string, additionalInfo: string = ""): void {
+        console.debug(
+            `LaborFormModel - ${action} of Form ${
+                this.thisFormRow !== null
+                    ? `Row ${this.thisFormRow + 1}`
+                    : "New Row"
+            }  Step : ${step} ${additionalInfo || ""}`
+        );
+    }
     // Populate the labor type dropdown
     populateLaborTypeDropdown(): void {
         const laborTypeOptions = Object.entries(this.laborItemDesc.enum).filter(
@@ -69,6 +120,61 @@ export class LaborFormModel {
         (document.getElementById(id) as HTMLInputElement).value = value;
     }
 
+    hideForm = (): void => {
+        this.trackStep("HIDE", "START");
+        this.formSection.style.display = "none";
+        this.resetForm();
+        this.trackStep("HIDE", "END");
+    };
+    showForm = (
+        action: "add" | "edit",
+        rowNumber: number | null = null
+    ): void => {
+        this.trackStep(
+            "SHOW",
+            "START",
+            `Row Number: ${rowNumber} and action: ${action}`
+        );
+        this.thisFormRow = rowNumber;
+        this.formSection.style.display = "block";
+        this.formTitle.textContent =
+            action === "add" ? "Add Labor Line" : "Edit Labor Line";
+        if (action === "edit" && rowNumber) {
+            const row = this.laborLines.rows[rowNumber - 1];
+            const previousRow = this.laborLines.rows[rowNumber];
+            this.trackStep(
+                "SHOW",
+                "START",
+                `Row ${row || "row is null"} ${
+                    previousRow || "previousRow is null"
+                }`
+            );
+            this.thisFormRow = rowNumber - 1;
+            Array.from(row.cells).forEach((cell, index) => {
+                console.log(
+                    `Cell Index: ${index},  Content: ${cell.textContent}`
+                );
+            });
+            Object.entries(this.formCellsMapping).forEach(([id, config]) => {
+                console.log(`Config: ${JSON.stringify(config)}`);
+                const cellValue = row.cells[config.index].textContent || "";
+                if (config.type === "select") {
+                    this.setInputValue(
+                        id,
+                        row.cells[config.index].textContent || ""
+                    );
+                } else {
+                    this.setInputValue(id, cellValue);
+                }
+                if ("editable" in config && !config.editable) {
+                    this.makeElementReadOnly(id);
+                }
+            });
+        } else {
+            this.resetForm();
+        }
+        this.trackStep("SHOW", "END");
+    };
     // Get the selected value and description from a dropdown
     getSelectedElement(id: string): { value: string; description: string } {
         const selectElement = document.getElementById(id) as HTMLSelectElement;
@@ -88,4 +194,109 @@ export class LaborFormModel {
             ("0" + inputDate.getMinutes()).slice(-2)
         );
     }
+    makeElementReadOnly = (id: string): void => {
+        const element = document.getElementById(id);
+        if (element) {
+            (element as HTMLInputElement).setAttribute("disabled", "true");
+        } else {
+            console.error(`Element with id ${id} not found.`);
+        }
+    };
+
+    makeElementEditable = (id: string): void => {
+        const element = document.getElementById(id);
+        if (element) {
+            (element as HTMLInputElement).removeAttribute("disabled");
+        } else {
+            console.error(`Element with id ${id} not found.`);
+        }
+    };
+    confirmForm = (): void => {
+        this.trackStep("CONFIRM", "START");
+        const values: string[] = Object.entries(this.formCellsMapping)
+            .map(([id, config]) => {
+                if (config.type === "select") {
+                    const { value, description } = this.getSelectedElement(id);
+                    return [description, value];
+                } else {
+                    return [this.getInputValue(id)];
+                }
+            })
+            .flat();
+
+        if (this.thisFormRow !== null) {
+            this.trackStep(
+                "CONFIRM",
+                "START",
+                `Editing row: ${this.thisFormRow}`
+            );
+            const row = this.laborLines.rows[this.thisFormRow];
+            values.forEach((value, i) => (row.cells[i].textContent = value));
+        } else {
+            // Validate if the row already exists using the formCellsMapping and elements with uniqueKey
+            const isDuplicate = Array.from(this.laborLines.rows).some((row) =>
+                Object.entries(this.formCellsMapping)
+                    .filter(
+                        ([id, config]) =>
+                            "uniqueKey" in config && config.uniqueKey
+                    )
+                    .every(([id, config]) => {
+                        if (config.type === "select") {
+                            return (
+                                row.cells[config.index].textContent ===
+                                this.getSelectedElement(id).value
+                            );
+                        } else {
+                            return (
+                                row.cells[config.index].textContent ===
+                                this.getInputValue(id)
+                            );
+                        }
+                    })
+            );
+
+            if (isDuplicate) {
+                console.error(
+                    "This combination already exists. Please edit the existing row"
+                );
+                alert(
+                    "This combination already exists. Please edit the existing row"
+                );
+                return;
+            }
+            this.trackStep("CONFIRM", "START", `Adding new row`);
+            const row = this.laborLines.insertRow();
+            values.forEach((value) => (row.insertCell().textContent = value));
+            this.trackStep(
+                "CONFIRM",
+                "PROCESS",
+                `Form will have the showForm : this.showForm("edit", ${row.rowIndex})`
+            );
+            const actionsCell = row.insertCell();
+            actionsCell.appendChild(
+                this.createButton("Edit", "btn btn-secondary", () =>
+                    this.showForm("edit", row.rowIndex)
+                )
+            );
+            actionsCell.appendChild(
+                this.createButton("Delete", "btn btn-danger", () =>
+                    this.laborLines.deleteRow(row.rowIndex - 1)
+                )
+            );
+        }
+        this.hideForm();
+        this.trackStep("CONFIRM", "END");
+    };
+
+    createButton = (
+        text: string,
+        className: string,
+        onClick: () => void
+    ): HTMLButtonElement => {
+        const button = document.createElement("button");
+        button.className = className;
+        button.textContent = text;
+        button.onclick = onClick;
+        return button;
+    };
 }
--- samples/016-ManageTeamWork/assets/js/custom.ts
+++ samples/016-ManageTeamWork/assets/js/custom.ts
@@ -4,6 +4,7 @@
  */
 
 import { OFSPlugin, OFSOpenMessage, OFSInitMessage } from "@ofs-users/plugin";
+import { LaborFormModel } from "./FormModel";
 // Create OFSOpenMessageCustom to extend OFSOpenMessage and include team
 interface OFSOpenMessageCustom extends OFSOpenMessage {
     team: any;
@@ -25,66 +26,12 @@ export class CustomPlugin extends OFSPlugin {
             "labor_item_number",
             JSON.stringify(message.attributeDescription.labor_item_number)
         );
-        console.info(
-            `${this.tag} : INIT labor_item_desc = ${this.getInitProperty(
-                "labor_item_desc"
-            )}`
-        );
-        console.info(
-            `${this.tag} : INIT labor_item_number = ${this.getInitProperty(
-                "labor_item_number"
-            )}`
-        );
         // check status of initproperties after evaluating init
     }
     open(_data: OFSOpenMessageCustom) {
         console.info(`${this.tag} :  OPEN`);
         // The list of possible technicians is based on team members
-        const populateTechnicianDropdown = (
-            _data: OFSOpenMessageCustom,
-            technicianSelect: HTMLSelectElement,
-            laborForm: HTMLFormElement
-        ): void => {
-            const teamMembers = _data.team?.teamMembers || {};
 
-            // Populate the technician dropdown with team member names
-            Object.entries(teamMembers).forEach(
-                ([key, member]: [string, any]) => {
-                    const option = document.createElement("option");
-                    option.value = member.external_id; // Capture external_id
-                    option.textContent = member.pname; // Show pname
-                    technicianSelect.appendChild(option);
-                }
-            );
-
-            // Add data.resource.pname and pid as an additional entry
-            if (_data.resource) {
-                const resourceOption = document.createElement("option");
-                resourceOption.value = _data.resource.pid; // Capture pid
-                resourceOption.textContent = _data.resource.pname; // Show pname
-                technicianSelect.appendChild(resourceOption);
-            }
-
-            // Add main resource to the form
-            const externalIdInput = document.createElement("input");
-            externalIdInput.type = "hidden";
-            externalIdInput.id = "external-id";
-            laborForm.appendChild(externalIdInput);
-
-            // Update external_id when technician changes
-            technicianSelect.onchange = () => {
-                const selectedOption =
-                    technicianSelect.options[technicianSelect.selectedIndex];
-                externalIdInput.value = selectedOption.value;
-            };
-        };
-        const gettimeinformaToSetHtml = (inputDate: Date): string => {
-            return (
-                ("0" + inputDate.getHours()).slice(-2) +
-                ":" +
-                ("0" + inputDate.getMinutes()).slice(-2)
-            );
-        };
         // Calculate default start and end time
         const getActivityTimeDatein24 = (
             _data: OFSOpenMessageCustom
@@ -172,143 +119,6 @@ export class CustomPlugin extends OFSPlugin {
                 ) as HTMLSelectElement,
             };
         };
-        const getInputValue = (id: string): string =>
-            (document.getElementById(id) as HTMLInputElement).value;
-        const getSelectedElement = (
-            id: string
-        ): { value: string; description: string } => {
-            const selectElement = document.getElementById(
-                id
-            ) as HTMLSelectElement;
-            const selectedOption =
-                selectElement.options[selectElement.selectedIndex];
-            return {
-                value: selectedOption.value,
-                description: selectedOption.textContent || "",
-            };
-        };
-
-        const setInputValue = (id: string, value: string): void => {
-            (document.getElementById(id) as HTMLInputElement).value = value;
-        };
-
-        const resetForm = (): void => {
-            // Console.log the labor form
-            console.debug(`${this.tag} Labor Form:  ${laborForm}`);
-            console.debug(
-                `${this.tag} Labor Form:  ${
-                    laborForm instanceof HTMLFormElement
-                }`
-            );
-            (laborForm as HTMLFormElement).reset();
-            // Adjust startTimeTxt and endTimeTxt to be just time compatible with start-time and end-time elements
-            // Select the last 8 charaters of the string startTimeTxt
-
-            setInputValue(
-                "start-time",
-                `${gettimeinformaToSetHtml(activityStartTimeDate)}`
-            );
-            setInputValue(
-                "end-time",
-                `${gettimeinformaToSetHtml(activityEndTimeDate)}`
-            );
-            console.debug(
-                `${
-                    this.tag
-                } Labor Form Modified with start and end time with PAD ${gettimeinformaToSetHtml(
-                    activityStartTimeDate
-                )} and ${gettimeinformaToSetHtml(activityEndTimeDate)}`
-            );
-            editIndex = null;
-        };
-
-        const showForm = (
-            action: "add" | "edit",
-            index: number | null = null
-        ): void => {
-            formSection.style.display = "block";
-            formTitle.textContent =
-                action === "add" ? "Add Labor Line" : "Edit Labor Line";
-
-            if (action === "edit" && index !== null) {
-                editIndex = index;
-                const row = laborLines.rows[index];
-                [
-                    "technician-name",
-                    "start-time",
-                    "end-time",
-                    "labor-type",
-                    "description",
-                ].forEach((id, i) =>
-                    setInputValue(id, row.cells[i].textContent || "")
-                );
-            } else {
-                resetForm();
-            }
-        };
-
-        const hideForm = (): void => {
-            formSection.style.display = "none";
-            resetForm();
-        };
-
-        const confirmForm = (): void => {
-            const { value: laborValue, description: laborDescription } =
-                getSelectedElement("labor-type");
-            const { value: techValue, description: techDescription } =
-                getSelectedElement("technician-name");
-
-            const values = [
-                techDescription,
-                techValue,
-                getInputValue("start-time"),
-                getInputValue("end-time"),
-                laborDescription,
-                laborValue,
-                getInputValue("description"),
-            ];
-            console.debug(
-                `${this.tag} Confirm Form: ${JSON.stringify(
-                    values
-                )} and editIndex: ${editIndex}`
-            );
-            if (editIndex !== null) {
-                const row = laborLines.rows[editIndex];
-                values.forEach(
-                    (value, i) => (row.cells[i].textContent = value)
-                );
-            } else {
-                const row = laborLines.insertRow();
-                values.forEach(
-                    (value) => (row.insertCell().textContent = value)
-                );
-
-                const actionsCell = row.insertCell();
-                actionsCell.appendChild(
-                    createButton("Edit", "btn btn-secondary", () =>
-                        showForm("edit", row.rowIndex - 1)
-                    )
-                );
-                actionsCell.appendChild(
-                    createButton("Delete", "btn btn-danger", () =>
-                        laborLines.deleteRow(row.rowIndex - 1)
-                    )
-                );
-            }
-            hideForm();
-        };
-
-        const createButton = (
-            text: string,
-            className: string,
-            onClick: () => void
-        ): HTMLButtonElement => {
-            const button = document.createElement("button");
-            button.className = className;
-            button.textContent = text;
-            button.onclick = onClick;
-            return button;
-        };
         const {
             laborLines,
             formSection,
@@ -333,8 +143,6 @@ export class CustomPlugin extends OFSPlugin {
             );
             return;
         }
-        populateTechnicianDropdown(_data, technicianSelect, laborForm);
-
         // Eliminate key from laborItemDesc.enum that are not in laborItemNumber.enum
         laborItemDesc.enum = Object.fromEntries(
             Object.entries(laborItemDesc.enum).filter(
@@ -342,27 +150,49 @@ export class CustomPlugin extends OFSPlugin {
                     laborItemNumber.enum[key] !== undefined
             )
         );
-        // Populate labor-type options with the values from the laborItemDesc.enum dict. the key will be the value and the text will be .text
-        // only when .inactive is null or false
-
-        const laborTypeOptions: any = Object.entries(laborItemDesc.enum).filter(
-            ([key, value]: [string, any]) => !value.inactive
-        );
-        laborTypeOptions.forEach(([key, value]: [string, any]) => {
-            const option = document.createElement("option");
-            option.value = key;
-            option.textContent = value.text;
-            laborTypeSelect.appendChild(option);
-        });
-
-        let editIndex: number | null = null;
-
         const { activityStartTimeDate, activityEndTimeDate } =
             getActivityTimeDatein24(_data);
+        const laborFormModel = new LaborFormModel(
+            laborLines,
+            laborForm,
+            formSection,
+            formTitle,
+            technicianSelect,
+            laborTypeSelect,
+            laborItemDesc,
+            laborItemNumber,
+            activityStartTimeDate,
+            activityEndTimeDate
+        );
+
+        // Example usage
+        laborFormModel.resetForm();
+        laborFormModel.populateTechnicianDropdown(
+            _data.team?.teamMembers,
+            _data.resource
+        );
+        laborFormModel.populateLaborTypeDropdown();
 
         // Assign the right functions to the buttons in the form
-        document.getElementById("add-button")!.onclick = () => showForm("add");
-        document.getElementById("hide-button")!.onclick = hideForm;
-        document.getElementById("confirm-button")!.onclick = confirmForm;
+        document.getElementById("add-button")!.onclick = () => {
+            laborFormModel.showForm("add");
+        };
+        document.getElementById("hide-button")!.onclick = () => {
+            laborFormModel.hideForm();
+        };
+        document.getElementById("confirm-button")!.onclick = () => {
+            // console.log that I have clicked on confirm button
+            laborFormModel.confirmForm();
+        };
+
+        document.getElementById("debrief-button")!.onclick = () => {
+            this.close();
+        };
+        document.getElementById("save-button")!.onclick = () => {
+            this.close();
+        };
+        document.getElementById("cancel-button")!.onclick = () => {
+            this.close();
+        };
     }
 }
