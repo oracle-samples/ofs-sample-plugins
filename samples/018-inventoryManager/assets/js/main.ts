e60cb91b4f10990a5210c6c9c464c146b3e53a22
Miguel Gall Martin <gall.miquel@gmail.com>
2025-09-04 16:01:25 -0400

    Add complete inventory manager plugin for OFS provider inventory management (#102)
    * Add complete inventory manager plugin for OFS provider inventory management
    
    * adding files to gitignore
    
    * some adjustments
    
    * add invsn on the fields to be shown on the table
    
    * working version

---
+++ samples/018-inventoryManager/assets/js/main.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2022, 2023, Oracle and/or its affiliates.
+ * Licensed under the Universal Permissive License (UPL), Version 1.0  as shown at https://oss.oracle.com/licenses/upl/
+ */
+
+import { CustomPlugin } from "./custom";
+import "../css/inventory.scss";
+
+export {};
+declare global {
+    interface Window {
+        ofs: CustomPlugin;
+    }
+}
+
+window.onload = function () {
+    window.ofs = new CustomPlugin("inventoryManager");
+};
